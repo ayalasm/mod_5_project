9de624c0fad9ff454e3fb6468b874028dcab3844
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-03-14 09:39:45 -0400

    add first API call and algo

--- ETL.py
+++ ETL.py
@@ -21,9 +21,7 @@ HELPERS FOR API CALL AND PARSING
 --------------------------------
 """
 
-# UPDATE THIS EVERY TIME WE NEED A NEW KIND OF API CALL
-
-def api_call(kind, params = None, id_ = None, token):
+def api_call(kind, token, params = None, id_ = None):
     """
     RETURNS
     -------
@@ -116,3 +114,36 @@ def parse_tracks(tracks_results):
     result['release_date'] = tracks_results['album']['release_date']
 
     return result
+
+"""
+API CALLS AND PARSING
+---------------------------
+
+1. Start with artist names to get Spotify artist IDs
+2. Using artist IDs, get up to 50 album IDs per artist
+3. Pass Album IDs into album_tracks call to get each individual track's info
+4. Pass track ids into analysis, features, and track info calls
+5. Maybe MySQL tables? Then can combine necessary things when you want?
+"""
+
+# Manually pick artists whose music we'll explore. 4 Salsa, 4 Bachata.
+artists = ["Oscar D'León", "Frankie Ruiz", "Tito Nieves", "La Maxima 79",
+               "Aventura", "Monchy & Alexandra", "Prince Royce", "Romeo Santos"]
+
+# Container to store their ids
+artist_ids = {}
+
+# Iterate through artist names
+for artist in artists:
+
+    # Set search params.
+    params = {
+          'q':f'{artist}',   # Search Query: [str] spaces must be separated by %20 or +
+          'type':'artist',   # Type of response: [str | comma sep optional] album, artist, playlist, and track
+          'limit':None,      # No. of responses: [int] default is 20. Can be (1, 50)
+          'offset':None      # Index of where to start in the search results. Can be (1, 100,000)
+    }
+
+    # Append entry to our dict with {artist_name : artist_id}
+    search_results = api_call('search', token, params)
+    artist_ids[artist] = parse_search(search_results)
