4ff14270f36e03fb94385ba09c82a805235b7774
Marco Sanchez-Ayala <sanchezayala.marco@gmail.com>
2020-05-21 10:54:45 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "salsa-bachata-classifier",
+    version = "0.1.0",
+    description = "A binary classifier to distinguish between salsa and bachata using Spotify's API",
+    author = "Marco Sanchez-Ayala",
+    packages = find_packages("src"),
+    package_dir = {"": "src"},
+    author_email = "sanchezayala.marco@gmail.com",
+    install_requires = [
+        "numpy==1.18.2",
+        "pandas==0.25.3",
+        "pytest==5.3.2",
+        "spotipy==2.11.2",
+    ],
+)
